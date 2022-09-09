546536300eeda5a941d17fe3acf7ff69897606f9
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-09-09 12:28:51 +0200

    dd

--- 4tiSP/4tiSP_prog/app_web/cw1/dist/index.js
+++ 4tiSP/4tiSP_prog/app_web/cw1/dist/index.js
@@ -4,5 +4,30 @@ document.querySelector("form")?.addEventListener("submit", (e) => {
     return false;
 });
 document.querySelector("#gener")?.addEventListener("click", (e) => {
-    alert(e.target);
+    document.querySelector("#info").innerHTML = "";
+    const quantity = document.querySelector("#ile");
+    const text = document.querySelector("#tekst");
+    let intOK = ValidateInt(quantity);
+    let textOK = ValidateText(text);
+    if (intOK && quantity != null && text != null && textOK) {
+        document.querySelector("#info")?.appendChild(GenerList(quantity, text));
+    }
 });
+function GenerList(q, text, kind = 'ol') {
+    const list = document.createElement("ol");
+    for (let i = 0; i < parseInt(q.value); i++) {
+        const li = document.createElement("li");
+        li.textContent = text.value;
+        list.appendChild(li);
+    }
+    return list;
+}
+function ValidateInt(elem) {
+    if (elem != null && (parseInt(elem.value) < 1 || (parseInt(elem.value) > 100)))
+        return false;
+    return true;
+}
+function ValidateText(text) {
+    let path = /^[A-ZŁÓŻŚĆŹŃĄĘ]{1}[A-Za-z\s].*/;
+    return text != null ? path.test(text?.value) : false;
+}
--- 4tiSP/4tiSP_prog/app_web/cw1/src/index.ts
+++ 4tiSP/4tiSP_prog/app_web/cw1/src/index.ts
@@ -1,11 +1,37 @@
-document.querySelector("form")?.addEventListener("submit",(e)=>{
+document.querySelector("form")?.addEventListener("submit", (e) => {
     e.preventDefault();
     return false;
 });
-document.querySelector("#gener")?.addEventListener("click",(e)=>{
-    alert(e.target);
+document.querySelector("#gener")?.addEventListener("click", (e) => {
+    (document.querySelector("#info") as HTMLElement).innerHTML="";
+    const quantity:HTMLInputElement|null = document.querySelector("#ile");
+    const text:HTMLInputElement|null = document.querySelector("#tekst");
+    let intOK = ValidateInt(quantity); 
+    let textOK = ValidateText(text);   
+    if(intOK && quantity!=null && text!=null && textOK){        
+        document.querySelector("#info")?.appendChild(GenerList(quantity,text));
+    }
+        
+    
+    
 })
 
-function ddd(){
-    
+function GenerList(q:HTMLInputElement,text:HTMLInputElement,kind='ol'):HTMLElement {
+    const list = document.createElement("ol");
+    for(let i=0;i<parseInt(q.value);i++){
+        const li = document.createElement("li");
+        li.textContent = text.value;
+        list.appendChild(li);
+    }
+    return list;
+}
+function ValidateInt(elem: HTMLInputElement | null): boolean {
+    if (elem != null && (parseInt(elem.value) < 1 || (parseInt(elem.value) > 100)))
+        return false;
+    return true;
+}
+
+function ValidateText(text: HTMLInputElement | null):boolean {
+   let path = /^[A-ZŁÓŻŚĆŹŃĄĘ]{1}[A-Za-z\s].*/
+   return text != null ? path.test(text?.value):false;
 }
