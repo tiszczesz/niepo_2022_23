eaa0ef531442c7ae19c19596807c94a846f0335b
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-09-15 12:41:04 +0200

    Update cw2.kt

--- 4tiGIM/mobile/android_projects/First_2022_23_4TIG/app/src/main/java/exercises/cw2.kt
+++ 4tiGIM/mobile/android_projects/First_2022_23_4TIG/app/src/main/java/exercises/cw2.kt
@@ -1,13 +1,15 @@
 package exercises
 
-import java.util.Scanner;
+import java.util.*
+import kotlin.random.Random.Default.nextInt
 
 
 fun main(){
     //cw21();
     //cw22();
     //cw23();
-    print(isPrimal(21));
+    //print(isPrimal(21));
+    cw24();
 }
 
 fun cw21(){
@@ -36,6 +38,27 @@ fun cw23(){
     println("Rozmiar tablicy: "+t1.size);
     for (i in t1){
         println(i);
+    }
+}
+fun cw24(){
+    val sc = Scanner(System.`in`);
+    print("Podaj rozmiar tablicy: ");
+    val size = sc.nextInt();
+    val rnd = Random();
+    val numbers = IntArray(size)
+    for (i in 0..size-1){
+        //numbers[i]=(2..Int.MAX_VALUE).random()
+        numbers[i] = nextInt(2,200)
+
+    }
+    val primals = numbers.filter { isPrimal(it) }
+    println("wszystkie")
+    for(i in numbers){
+        print(i.toString()+" ")
+    }
+    println("\n\tpierwsze")
+    for (i in primals){
+        print(i.toString()+" ")
     }
 }
 fun isPrimal(number:Int):Boolean {
