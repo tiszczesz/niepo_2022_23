1be4b1ec7f6b9a4716c815aaf7a83283bf5a0609
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-09-23 14:07:03 +0200

    mm

--- 2tip/2ti_desk/cw4/Book.cs
+++ 2tip/2ti_desk/cw4/Book.cs
@@ -3,6 +3,7 @@ class Book{
     private string? title = "Wlasny tytul";
     public string? Title {
         get{ return title.ToUpper();}
+        set{ title = value;}
     }
     private decimal price = 33.89M;
     public decimal Price{
@@ -12,7 +13,30 @@ class Book{
     private string? author = "Roman Boman";
     public string? Author{
         get{return author;}
+        set{ author = value;}
     }
 
     //metody klasy
+    public Book()
+    {
+        Title = "noname";
+        Author = "noname";
+        Price = 0;
+    }
+      public Book(string title,string author,decimal price=10.99M)
+    {
+        Title = title;
+        Author = author;
+        Price = price;
+    }
+    public override string ToString(){
+        return $"Ksiazka: {Title} napisana przez: {Author} w cenie: {Price} PLN";
+    }
+    
+}
+class Test{
+    public override string ToString()
+    {
+        return " cos wlasnego do metody toString";
+    }
 }
--- 2tip/2ti_desk/cw4/Program.cs
+++ 2tip/2ti_desk/cw4/Program.cs
@@ -1,4 +1,7 @@
 ﻿Book b1 = new Book();
 b1.Price = -34.90M;
-
-Console.WriteLine($"Ksiazka: {b1.Title} napisana przez: {b1.Author} w cenie: {b1.Price} PLN");
+Book b2 = new Book("ABC czegos tam","Xawery Bebery",399.00M);
+Console.WriteLine(b1);
+Console.WriteLine(b2);
+Test t1 = new Test();
+Console.WriteLine(t1);
