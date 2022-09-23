4618ba854d5268f43e719c833d215fbfbd66ef9d
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-09-23 13:24:39 +0200

    gg

--- 4tiGIM/mobile/android_projects/First_2022_23_4TIG/app/src/main/java/com/niepolomice/first_2022_23_4tig/MainActivity.kt
+++ 4tiGIM/mobile/android_projects/First_2022_23_4TIG/app/src/main/java/com/niepolomice/first_2022_23_4tig/MainActivity.kt
@@ -2,10 +2,22 @@ package com.niepolomice.first_2022_23_4tig
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.Toast
+import kotlin.math.log
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val obrazek = findViewById<ImageView>(R.id.imageView1)
+        Log.d("TAAAAG",obrazek.toString())
+        obrazek.setOnClickListener {
+            Log.d("ffff","fffffffff")
+            Toast.makeText(this@MainActivity,"Jestem zebra ",Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
