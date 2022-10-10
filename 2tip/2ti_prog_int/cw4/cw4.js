6d2e46dca43844a99b9e56ae779f973b4b8545e4
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-10-10 08:45:10 +0200

    mm

--- 2tip/2ti_prog_int/cw4/cw4.js
+++ 2tip/2ti_prog_int/cw4/cw4.js
@@ -1,18 +1,39 @@
-function Book(title,price,author,category){
+function Book(title, price, author, category) {
     this.title = title;
     this.price = price;
     this.author = author;
     this.category = category;
-    this.ShowInPar = function(){
+    this.ShowInPar = function () {
         return `<p>${this.title} ${this.author} ${this.category} cena: ${this.price} PLN</p>`;
+    }
+    this.ShowInRow = function (lp) {
+        return `<tr>
+            <td>${lp}</td>
+            <td>${this.title}</td> 
+            <td>${this.author}</td> 
+            <td>${this.category}</td> 
+            <td  class="text-end">${this.price.toFixed(2)}</td>
+        </tr>`;
     }
 }
 const books = [
-    new Book("Góry polskie",23.99,"Ksawery Bery","podróżnicze"),
-    new Book("Wstęp do JS",78.69,"Monika Lewicka","programowanie"),
-    new Book("Zaawansowane PHP",123.99,"Monika Lewicka","podróżnicze"),
-    new Book("Książka kucharska",53.00,"Roman Boman","poradnik"),
-    new Book("Jeziora mazurskie",45.99,"Ksawery Bery","podróżnicze")
+    new Book("Góry polskie", 23.99, "Ksawery Bery", "podróżnicze"),
+    new Book("Wstęp do JS", 78.69, "Monika Lewicka", "programowanie"),
+    new Book("Zaawansowane PHP", 123.99, "Monika Lewicka", "podróżnicze"),
+    new Book("Książka kucharska", 53.00, "Roman Boman", "poradnik"),
+    new Book("Jeziora mazurskie", 45.99, "Ksawery Bery", "podróżnicze")
 ]
+function GenerTab(data,className){
+    let html = `<table class='${className}'>`;
+    let lp = 0;
+    html += '<tr><th>Lp</th><th>Tytuł</th><th>Autor</th><th>Gatunek</th><th>Cena</th></tr>';
+    data.forEach(element => {
+        html += element.ShowInRow(++lp);
+    });
+
+    return html+"</table>";
+}
+document.querySelector("#root").innerHTML = GenerTab(books,'table');
+
 
 console.log(books);
