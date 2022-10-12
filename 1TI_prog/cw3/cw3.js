c0b113a7b314f2a82bc2eb3d42b0fe0013fa9b62
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-10-12 12:22:39 +0200

    dd

--- 1TI_prog/cw3/cw3.js
+++ 1TI_prog/cw3/cw3.js
@@ -1,6 +1,12 @@
 const a = parseFloat(prompt("podaj a"));
 const b = parseFloat(prompt("podaj b"));
 document.querySelector("#info").innerHTML = Hello("Franek");
+const element = document.querySelector("#info4");
+if(element!=null){
+    setInterval(function(){
+        changeColor(element);
+    },200);
+}
 //Add("#info2",a,b);
 Add("#info3",a,b);
 function Hello(text){
@@ -14,4 +20,11 @@ function Add(selector,liczba1,liczba2){
         return;
     }
     elem.innerHTML = `${liczba1} + ${liczba2} = ${liczba1+liczba2}`;
+}
+function changeColor(elem){
+    //console.log(elem);
+    let red = Math.floor(Math.random()*256);
+    let green = Math.floor(Math.random()*256);
+    let blue = Math.floor(Math.random()*256);
+    elem.style.color = `rgb(${red},${green},${blue})`;
 }
--- 4tiGIM/mobile/android_projects/MyLinearLayout/app/src/main/java/com/niepolomice/mylinearlayout/MainActivity.kt
+++ 4tiGIM/mobile/android_projects/MyLinearLayout/app/src/main/java/com/niepolomice/mylinearlayout/MainActivity.kt
@@ -4,22 +4,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     var licznik = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var btnLike = findViewById<Button>(R.id.btnLike)
         var btnReset = findViewById<Button>(R.id.btnReset)
         var textLike = findViewById<TextView>(R.id.likeNumber)
+        var btnSave = findViewById<Button>(R.id.btnSave)
         btnLike.setOnClickListener {
             licznik++
             textLike.text = licznik.toString()+" polubień"
         }
         btnReset.setOnClickListener {
             licznik = if (licznik>0) licznik-1 else licznik
-            textLike.text = licznik.toString()+" polubień"
+            UpdateLikes(textLike)
         }
+        val sharePref = getSharedPreferences("myPref", MODE_PRIVATE);
+        val editor = sharePref.edit();
+        licznik = sharePref.getInt("licznik",0);
+        UpdateLikes(textLike);
+        btnSave.setOnClickListener {
+            editor.putInt("licznik",licznik);
+            editor.apply();
+            Toast.makeText(this@MainActivity,"Zapisano",Toast.LENGTH_SHORT).show();
+        }
+    }
+    fun UpdateLikes(view:TextView){
+        view.text =  licznik.toString()+" polubień";
     }
 }
