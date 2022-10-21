977e7bb95b3696abba7c460645070827ab24e4d4
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-10-21 14:50:25 +0200

    gg

--- 1TI_prog/cw4/cw4.js
+++ 1TI_prog/cw4/cw4.js
@@ -1,21 +1,32 @@
 
-document.querySelector("#run").addEventListener("click",function(){
+document.querySelector("#run").addEventListener("click", function () {
     console.log("click");
-    
-    let firstname = document.querySelector("#firstname").value;
-    let lastname = document.querySelector("#lastname").value;
-    let division = document.querySelector("#division").value;
-    document.querySelector("#info").innerHTML = SetInfo(firstname,lastname,division);
-    ClearForm();
+    let firstname = document.querySelector("#firstname");
+    let lastname = document.querySelector("#lastname");
+    let division = document.querySelector("#division");
+    document.querySelector("#info").innerHTML = SetInfo(firstname, lastname, division);
+   // ClearForm();
     //document.querySelector("#info").innerHTML = firstname;
 });
 
-function ClearForm(){
+function ClearForm() {
     document.querySelector("#firstname").value = "";
     document.querySelector("#lastname").value = "";
     document.querySelector("#division").value = "";
 }
-function SetInfo(fnamem,lname,divname){
-    if(fnamem.trim()==='' || lname.trim()==='' || divname.trim()==='' ) return "BRAK LUB NIEPEŁNE DANE!!!"
-    return `<div>Imię: ${fnamem}</div><div>Nazwisko: ${lname}</div><div>Klasa: ${divname}</div>`
+function SetInfo(fnamem, lname, divname) {
+    const firstNameOK = ValidateInput(fnamem);
+    const lastNameOK = ValidateInput(lname);
+    const divisionOK = ValidateInput(divname);
+    if (!firstNameOK || !lastNameOK || !divisionOK) return "BRAK LUB NIEPEŁNE DANE!!!"
+    return `<div>Imię: ${fnamem.value}</div><div>Nazwisko: ${lname.value}</div><div>Klasa:
+                                     ${divname.value}</div>`
+}
+function ValidateInput(element) {
+    if (element.value.trim() == '') {
+        element.className = "error";
+        return false;
+    }
+    element.className = "ok";
+    return true;
 }
