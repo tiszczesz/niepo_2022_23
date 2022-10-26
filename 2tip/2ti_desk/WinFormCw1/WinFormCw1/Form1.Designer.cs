75e160ef5d3c89599e52c596eb761bef048101c4
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-10-26 09:53:04 +0200

    rr

--- 2tip/2ti_desk/WinFormCw1/WinFormCw1/Form1.Designer.cs
+++ 2tip/2ti_desk/WinFormCw1/WinFormCw1/Form1.Designer.cs
@@ -28,7 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblStart = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblStart
+            // 
+            this.lblStart.AutoSize = true;
+            this.lblStart.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblStart.ForeColor = System.Drawing.Color.Blue;
+            this.lblStart.Location = new System.Drawing.Point(79, 32);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(96, 37);
+            this.lblStart.TabIndex = 0;
+            this.lblStart.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(103, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(103, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 37);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(103, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 37);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(454, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(290, 65);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Sprawdź Czas";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.lblTime.Location = new System.Drawing.Point(572, 150);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(0, 15);
+            this.lblTime.TabIndex = 5;
             // 
             // Form1
             // 
@@ -36,12 +106,27 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(984, 661);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblStart);
             this.Name = "Form1";
             this.Text = "Główne okno";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label lblStart;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private Label lblTime;
     }
 }
--- 2tip/2ti_desk/WinFormCw1/WinFormCw1/Form1.cs
+++ 2tip/2ti_desk/WinFormCw1/WinFormCw1/Form1.cs
@@ -6,5 +6,20 @@ namespace WinFormCw1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblStart.Text = "Hello from WindowsForms";
+            // ................
+            label1.Text = "Antoni Ma³ecki";
+            label2.Text = "Klasa 2TI";
+            label3.Text = DateTime.Now.Year.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string time = DateTime.Now.ToLocalTime().ToString();
+            lblTime.Text = time;
+        }
     }
 }
