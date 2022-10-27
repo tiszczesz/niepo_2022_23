243be41661148a6cd45aa7583fcccb2716448514
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-10-27 14:17:58 +0200

    hh

--- 4tiGIM/webowe/cw6-react/src/App.tsx
+++ 4tiGIM/webowe/cw6-react/src/App.tsx
@@ -2,12 +2,19 @@ import Range from './Range';
 import './App.css';
 
 function App() {
-  
+  const colors = [255,255,255];
+  const setColors = (index:number,value:number) =>{
+    colors[index] = value;
+    console.log(colors)
+    document.body.style.backgroundColor = `rgb(${colors[0]},${colors[1]},${colors[2]})`
+  }
+
   return (
     <div className='container'>        
-        <Range colorProps='red'/>
-        <Range colorProps='green'  />
-        <Range colorProps='blue' />
+        <Range colorProps='red' handlerChange={setColors} indexOfColor={0}/>
+        <Range colorProps='green'  handlerChange={setColors} indexOfColor={1}/>
+        <Range colorProps='blue' handlerChange={setColors} indexOfColor={2}/>
+      
     </div>
 
   
--- 4tiGIM/webowe/cw6-react/src/Range.tsx
+++ 4tiGIM/webowe/cw6-react/src/Range.tsx
@@ -3,18 +3,20 @@ import "./Range.css"
 
 type RangeProps = {
     colorProps:string;
-    
+    indexOfColor:number
+    handlerChange:(index:number,value:number)=>void
 }
 
 const Range = (props:RangeProps) => {
-    const [info,setInfo] = useState("0")
+    const [info,setInfo] = useState("255")
     
     return(
         <div className="border">
         <div style={{color:props.colorProps}}>Kolor {props.colorProps}</div>
         <input onChange={(e)=>{
             setInfo(e.target.value);
-           
+            console.log(`info=${e.target.value}`);
+            props.handlerChange(props.indexOfColor,parseInt(e.target.value))
         }} type="range" min={0} max={255} value={info} />
         <label>{info}</label>
         </div>
