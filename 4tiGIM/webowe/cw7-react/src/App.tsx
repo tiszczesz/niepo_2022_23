8705ec9d1271c219538e5e14731842ed5dab1db9
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-11-02 16:42:09 +0100

    gg

--- 4tiGIM/webowe/cw7-react/src/App.tsx
+++ 4tiGIM/webowe/cw7-react/src/App.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 
 function App() {
+  const [width,setWidth]=useState("100");
+  const [height,setHeight]=useState("100");
   return (
     <div className='container'>
       <div>
-        <input type="range" name="height" id="height" min={100} max={300} /><br />
-        <input type="range" name="width" id="width" min={100} max={300}/>
+        <input type="range" name="height" id="height" min={100} max={300} 
+        onChange={(e)=>{
+          setHeight(e.target.value)
+        }}
+        /><br />
+        <input type="range" name="width" id="width" min={100} max={300} 
+        onChange={(e)=>{
+          setWidth(e.target.value)
+        }}
+        />
       </div>
-      <div className='rectangle' style={{width:"200px",height:"300px"}}></div>
+      <div className='rectangle' style={{width:width+"px",height:height+"px"}}>
+        <span>{width} x {height}</span>
+        </div>
     </div>
   );
 }
