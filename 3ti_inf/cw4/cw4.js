2b8a38e9089165eb1c4bd3ead58bed2ea2610e8a
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-11-03 08:34:13 +0100

    gg

--- 3ti_inf/cw4/cw4.js
+++ 3ti_inf/cw4/cw4.js
@@ -1,1 +1,13 @@
-const defs = document.querySelectorAll("dt");
+const dts = document.querySelectorAll("dt");
+console.log(dts);
+for(let i=0;i<dts.length;i++){
+    dts[i].onclick = function(e){
+        console.log(e.target.nextElementSibling.innerHTML);
+        if(e.target.nextElementSibling.style.display != "block"){
+            e.target.nextElementSibling.style.display = "block";
+        }else{
+            e.target.nextElementSibling.style.display = "none";
+        }
+        
+    }
+}
