0758521ec6df25534b31dd18da77bf886124c334
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-11-04 12:42:54 +0100

    hh

--- 4tiSP/4tiSP_prog/app_web/cw4_webpack1/src/index.ts
+++ 4tiSP/4tiSP_prog/app_web/cw4_webpack1/src/index.ts
@@ -1,6 +1,17 @@
 console.log("Hello from Webpack");
+
+const Users = ["Admin","Prof","Student","Inny","Winny"];
 const app = document.querySelector("#app");
+
+const ol = document.createElement("ol");
+ol.className = "list-group";
+Users.forEach((e,i)=>{
+    const li = document.createElement("li");
+    li.innerHTML = e;
+    li.className = "list-group-item";
+    ol.appendChild(li);
+})
 if(app!==null){
     app.innerHTML = "Teraz coś nowego";
+    app.appendChild(ol);
 }
-;
