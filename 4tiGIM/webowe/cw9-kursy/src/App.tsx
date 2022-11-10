3913f8a5857d14bcb36e81b57d4afa332f8bd185
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-11-10 08:23:32 +0100

    ggg

--- 4tiGIM/webowe/cw9-kursy/src/App.tsx
+++ 4tiGIM/webowe/cw9-kursy/src/App.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import {Courses } from './dane';
+import { Courses } from './dane';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+const App = () => {
   return (
-    <p>{Courses[Courses.length-1].info()}</p>
+    <div className="container">
+      <h1>Informacje o kursach</h1>
+      <div>
+        <table className="table table-bordered">
+          <tr>
+            <th>Nr. kursu</th>
+            <th>Tytuł</th>
+            <th >Data</th>
+            <th>Miejsce</th>
+          </tr>
+          {Courses.map((elm,i)=>(
+            <tr>
+              <td>{elm.id}</td>
+              <td>{elm.name}</td>
+              <td >{elm.date.toLocaleDateString()}</td>
+              <td>{elm.place}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    </div>
   );
 }
 
