50e5713493a902b74a8e0e8ceb8f7ef47cb9c0aa
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-11-10 15:58:05 +0100

    dd

--- 3TI_prog/webowe/OgloszeniaMVCMysql/OgloszeniaMVCMysql/Controllers/AdsController.cs
+++ 3TI_prog/webowe/OgloszeniaMVCMysql/OgloszeniaMVCMysql/Controllers/AdsController.cs
@@ -5,18 +5,23 @@ using OgloszeniaMVCMysql.ViewModels;
 
 namespace OgloszeniaMVCMysql.Controllers
 {
-    public class AdsController : Controller {
+    public class AdsController : Controller
+    {
         private MyAppContext _db;
 
-        public AdsController(MyAppContext db) {
+        public AdsController(MyAppContext db)
+        {
             _db = db;
         }
-        public IActionResult Index(int id) {
+        public IActionResult Index(int id)
+        {
             IndexViewModel indexViewModel = new IndexViewModel();
             indexViewModel.Kategories = _db.Kategories.ToList();
-            indexViewModel.OgloszeniaByCat = id == -1 ? _db.AllOgloszenia.ToList()
-                : _db.AllOgloszenia.Where(o => o.uzytkownik_id == id).ToList();
-            indexViewModel.Uzytkownicies = _db.AllUzytkownicy.Where(k => k.id < 4).Include(u=>u.Ogloszenias).ToList();
+            indexViewModel.OgloszeniaByCat = id == -1 ? _db.AllOgloszenia
+                    .Include(o => o.Kategorie).ToList()
+                : _db.AllOgloszenia.Include(o => o.Kategorie).Where(o => o.kategoria == id).ToList();
+            indexViewModel.Uzytkownicies = _db.AllUzytkownicy
+                .Where(k => k.id < 4).Include(u => u.Ogloszenias).ToList();
             return View(indexViewModel);
         }
     }
--- 3TI_prog/webowe/OgloszeniaMVCMysql/OgloszeniaMVCMysql/Models/Ogloszenia.cs
+++ 3TI_prog/webowe/OgloszeniaMVCMysql/OgloszeniaMVCMysql/Models/Ogloszenia.cs
@@ -11,10 +11,13 @@ namespace OgloszeniaMVCMysql.Models
         
         [ForeignKey(nameof(Uzytkownicy))]
         public int uzytkownik_id { get; set; }
+
+        [ForeignKey(nameof(Kategorie))]
         public int kategoria { get; set; }
         public int podkategoria { get; set; }
         public string tytul { get; set; }
         public string tresc { get; set; }
         public Uzytkownicy Uzytkownicy { get; set; }
+        public Kategorie Kategorie { get; set; }
     }
 }
