e74fbb0fcc82520c97cd38711118aff0d1d558f1
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-11-14 08:35:02 +0100

    gg

--- 3ti_inf/cw6/cw6.js
+++ 3ti_inf/cw6/cw6.js
@@ -1,8 +1,15 @@
 console.log("sdsdfsf");
-document.querySelector("#napis").onchange = function(e){
-    console.log("onchange: "+e.target);
+document.querySelector("#napis").onchange = function (e) {
+    console.log("onchange: " + e.target);
 };
-document.querySelector("#napis").oninput = function(e){
-    console.log("oninput: "+e.target);
-    document.querySelector("#result1").innerHTML = e.target.value;
+document.querySelector("#napis").oninput = function (e) {
+    console.log("oninput: " + e.target);
+    document.querySelector("#result1").innerHTML = e.target.value.toUpperCase();
 }
+document.querySelector("#napis2").oninput = function(e){
+    if(e.target.value.trim().length>2){
+        e.target.classList.remove("error");
+    }else{
+        e.target.className = "error";
+    }
+};
