0017d509bfead846cf063a2e3b4a6781bde903b3
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-11-30 16:28:10 +0100

    eee

---
+++ 4tiGIM/webowe/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public List<Kurs> MojeKursy { get; set; } = MojeDane.NazwyKursow();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index() {
+            //MojeKursy = MojeDane.NazwyKursow();
+            return View(MojeKursy);
+        }
+
+        public IActionResult Kurs(int id) {
+            Kurs k1 = MojeKursy.Find(k=>k.Id==id);
+            return View(k1);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 4tiGIM/webowe/WebApplication1/WebApplication1/Models/MojeDane.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApplication1.Models
+{
+    public class MojeDane
+    {
+        public static List<Kurs> NazwyKursow() {
+            return new List<Kurs> {
+                new Kurs{Id = 1,Name = "Kurs pierwszy", Data = new DateTime(2022,12,20)},
+                new Kurs{Id = 2,Name = "Kurs drugi", Data = new DateTime(2022,11,20)},
+                new Kurs{Id = 3,Name = "Kurs trzeci", Data = new DateTime(2022,12,26)},
+                new Kurs{Id = 4,Name = "Kurs czwarty", Data = new DateTime(2023,12,20)},
+            };
+        }
+    }
+
+    public class Kurs {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Data { get; set; }
+    }
+}
