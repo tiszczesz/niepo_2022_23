8b53fcb2baaab83b2cb56af457b52069081b6c72
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-12-08 08:32:46 +0100

    gg

--- 3ti_inf/cw9/cw9.js
+++ 3ti_inf/cw9/cw9.js
@@ -20,8 +20,14 @@ document.querySelector("#oblicz").onclick = function(){
 }
 
 function silniaRek(n){
-
+    if(n<=1) return 1;
+    return silniaRek(n-1)*n;
 }
 function silniaIter(n){
-    
+    if(n<=1) return 1;
+    let result = 1;
+    for(let i = 1;i <= n; i++){
+        result = result * i;
+    }
+    return result;
 }
