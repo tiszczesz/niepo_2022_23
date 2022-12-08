f9be449a4b32c078f98fa77fff430aad25ed3407
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-12-08 08:27:59 +0100

    gg

--- 3ti_inf/cw9/cw9.js
+++ 3ti_inf/cw9/cw9.js
@@ -2,12 +2,18 @@ document.querySelector("#oblicz").onclick = function(){
     const n = parseInt(document.querySelector("#n").value);
     if(!isNaN(n)){
         const choice = document.querySelectorAll("input[type=radio]");
-        //console.log(choice);
+        const metoda2 = document.querySelector("input[type=radio]:checked").value;
+        console.log(metoda2);
         let metoda = ""
         for(let i=0; i<choice.length;i++){
             if(choice[i].checked) metoda = choice[i].value;
         }
         //console.log(metoda);
+        if(metoda==='iter'){
+            //silniaIter(n)
+        }else{
+            //silniaRek(n)
+        }
     }else{
         document.querySelector("#result").innerHTML = "Blędne dane";
     }
