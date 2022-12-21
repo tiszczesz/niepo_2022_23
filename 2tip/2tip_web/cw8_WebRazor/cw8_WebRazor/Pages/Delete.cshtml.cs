b1f597a68b17ba340196184209f0e43b16db04a0
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-12-21 10:34:43 +0100

    dd

---
+++ 2tip/2tip_web/cw8_WebRazor/cw8_WebRazor/Pages/Delete.cshtml.cs
@@ -0,0 +1,23 @@
+using cw8_WebRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cw8_WebRazor.Pages
+{
+    public class DeleteModel : PageModel
+    {
+        public int Id { get; set; }
+        public Contact Contact { get; set; }
+        public RepoContacts MyRepo { get; set; }
+
+        public DeleteModel() {
+            MyRepo = new RepoContacts();
+        }
+        public void OnGet(int? id)
+        {
+            if (id != null) {
+                Contact = MyRepo.Contacts.Find(c => c.Id == id);
+            }
+        }
+    }
+}
