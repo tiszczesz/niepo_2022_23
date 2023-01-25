66ae65ca1a72d258500bd4a619c5d50b3736b4c8
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-01-25 10:27:54 +0100

    dd

--- 2tip/2tip_web/cw8_WebRazor/cw8_WebRazor/Pages/Update.cshtml.cs
+++ 2tip/2tip_web/cw8_WebRazor/cw8_WebRazor/Pages/Update.cshtml.cs
@@ -7,19 +7,33 @@ namespace cw8_WebRazor.Pages
     public class UpdateModel : PageModel
     {
         [BindProperty] 
-        public Contact ContactToUpdate { get; set; }
+        public Contact ContactUpdated { get; set; }
 
         private RepoContacts _repo = new RepoContacts();
         public IActionResult OnGet(int? id) {
             if(id==null) new RedirectToPageResult("MyPage");
-            ContactToUpdate = _repo.GetById(id);
-            if (ContactToUpdate!=null) {
+            ContactUpdated = _repo.GetById(id);
+            if (ContactUpdated != null) {
                 return Page();
             }
             else {
                 return new RedirectToPageResult("MyPage");
             }
             
+        }
+
+        public IActionResult OnPost(int? id) {
+            if (ModelState.IsValid) {
+                Contact? contact = _repo.GetById(id);
+                if (contact != null) {
+                    contact.Email = ContactUpdated.Email;
+                    contact.Name = ContactUpdated.Name;
+                    contact.Phone = ContactUpdated.Phone;
+                    _repo.SaveToFile();
+                }
+                return new RedirectToPageResult("MyPage");
+            }
+            return Page();
         }
     }
 }
