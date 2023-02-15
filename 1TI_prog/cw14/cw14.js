bc18e2c9fe1d4d8bfb27f25deb45a054d91fd57f
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-02-15 12:40:57 +0100

    hh

--- 1TI_prog/cw14/cw14.js
+++ 1TI_prog/cw14/cw14.js
@@ -15,15 +15,19 @@ function GenerBook() {
     images.forEach((v, i) => {
         books.push(new Book(v[0], v[1], v[2], v[3]))
     });
+    return books;
 }
 
 const images = [
     ["C# 10 i .NET 6 dla programistów aplikacji wieloplatformowych.jpg"
         , "Antoni Małecki", 2021, 67.90],
     ["C++ Zbiór zadań z rozwiązaniami.jpg", "Roman Bałecki", 2019, 45.90],
-    ["Czysty Agile. Powrót do podstaw.jpg", "Roman Bałecki", 2019, 45.90],
-    ["Etyczny haking.jpg", "Roman Bałecki", 2019, 45.90],
+    ["Czysty Agile. Powrót do podstaw.jpg", "Roman Bałecki", 2019, 89.90],
+    ["Etyczny haking.jpg", "Roman Bałecki", 2017, 69.90],
     ["Git i GitHub. Kontrola wersji, zarządzanie projektami i zasady pracy zespołowej.jpg", "Roman Bałecki", 2019, 45.90],
     ["JavaScript i jQuery. Interaktywne strony WWW dla każdego. Podręcznik Front-End Developera.jpg", "Roman Bałecki", 2019, 45.90],
-    ["Czysty kod. Podręcznik dobrego programisty.jpg", "Roman Bałecki", 2019, 45.90]
+    ["Czysty kod. Podręcznik dobrego programisty.jpg", "Roman Bałecki", 2022, 33.90]
 ];
+
+const books = GenerBook();
+console.log(books);
