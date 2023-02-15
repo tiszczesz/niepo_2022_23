a7715b4612114bb686a91f0a6ae88adf0f489600
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-02-15 12:30:49 +0100

    gg

---
+++ 1TI_prog/cw14/cw14.js
@@ -0,0 +1,29 @@
+class Book {
+    constructor(image, author, year, price) {
+        this.title = this.GetTitle(image);
+        this.image = image;
+        this.author = author;
+        this.year = year;
+        this.price = price
+    }
+    GetTitle(image) {
+        return image.substring(0, image.lastIndexOf('.'));
+    }
+}
+function GenerBook() {
+    const books = [];
+    images.forEach((v, i) => {
+        books.push(new Book(v[0], v[1], v[2], v[3]))
+    });
+}
+
+const images = [
+    ["C# 10 i .NET 6 dla programistów aplikacji wieloplatformowych.jpg"
+        , "Antoni Małecki", 2021, 67.90],
+    ["C++ Zbiór zadań z rozwiązaniami.jpg", "Roman Bałecki", 2019, 45.90],
+    ["Czysty Agile. Powrót do podstaw.jpg", "Roman Bałecki", 2019, 45.90],
+    ["Etyczny haking.jpg", "Roman Bałecki", 2019, 45.90],
+    ["Git i GitHub. Kontrola wersji, zarządzanie projektami i zasady pracy zespołowej.jpg", "Roman Bałecki", 2019, 45.90],
+    ["JavaScript i jQuery. Interaktywne strony WWW dla każdego. Podręcznik Front-End Developera.jpg", "Roman Bałecki", 2019, 45.90],
+    ["Czysty kod. Podręcznik dobrego programisty.jpg", "Roman Bałecki", 2019, 45.90]
+];
