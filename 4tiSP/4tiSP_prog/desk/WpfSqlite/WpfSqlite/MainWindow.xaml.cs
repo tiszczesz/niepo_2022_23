d5411fe4b36127d21d6f52866f0ef6bbccfb0ffb
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-03-03 12:21:41 +0100

    gg

--- 4tiSP/4tiSP_prog/desk/WpfSqlite/WpfSqlite/MainWindow.xaml.cs
+++ 4tiSP/4tiSP_prog/desk/WpfSqlite/WpfSqlite/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +13,38 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.EntityFrameworkCore;
 
 namespace WpfSqlite
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
+    public partial class MainWindow : Window {
+        private readonly ProductContext _dbContext = new ProductContext();
+        private CollectionViewSource categoryViewSource;
         public MainWindow()
         {
             InitializeComponent();
+            categoryViewSource = (CollectionViewSource) FindResource(nameof(categoryViewSource));
+        }
+
+        private void ButtonSave(object sender, RoutedEventArgs e) {
+            _dbContext.SaveChanges();
+            categoryDataGrid.Items.Refresh();
+            productsDataGrid.Items.Refresh();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            //_dbContext.Database.EnsureCreated();
+            _dbContext.Categories.Load();
+            categoryViewSource.Source = 
+                _dbContext.Categories.Local.ToObservableCollection();
+        }
+
+        protected override void OnClosing(CancelEventArgs e) {
+            _dbContext.Dispose();
+            base.OnClosing(e);
         }
     }
 }
