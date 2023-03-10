d5b9395ba4dbda80374e37c320a09843847fb812
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-03-10 09:25:15 +0100

    ddd

---
+++ 2tip/2tip_web/MVC_cw2/MVC_cw2/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_cw2.Models;
+using System.Diagnostics;
+
+namespace MVC_cw2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Hello() {
+            return "Hello from MVC";
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
