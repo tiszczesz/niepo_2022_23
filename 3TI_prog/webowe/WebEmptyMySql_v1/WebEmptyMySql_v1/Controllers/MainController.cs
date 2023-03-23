a8ef3a9130cc5a13129b8aeb730d001e22e9e6dd
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-03-23 15:06:19 +0100

    gg

--- 3TI_prog/webowe/WebEmptyMySql_v1/WebEmptyMySql_v1/Controllers/MainController.cs
+++ 3TI_prog/webowe/WebEmptyMySql_v1/WebEmptyMySql_v1/Controllers/MainController.cs
@@ -1,12 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebEmptyMySql_v1.Models;
 
 namespace WebEmptyMySql_v1.Controllers
 {
-    public class MainController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
+    public class MainController : Controller {
+        private string connString;
+
+        public MainController(IConfiguration configuration) {
+            this.connString = configuration.GetConnectionString("MyConn");
+        }
+        public IActionResult Index() {
+            CarsRepo repo = new CarsRepo(connString);
+            var cars = repo.GetCars();
+            return View(cars);
         }
     }
 }
---
+++ 3TI_prog/webowe/WebEmptyMySql_v1/WebEmptyMySql_v1/Models/CarsRepo.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace WebEmptyMySql_v1.Models
+{
+    public class CarsRepo {
+        private string connString;
+
+        public CarsRepo(string conn) {
+            this.connString = conn;
+        }
+
+        public List<Car> GetCars() {
+            List<Car> cars = new List<Car>();
+            using (MySqlConnection conn = new MySqlConnection(connString)) {
+                conn.Open();
+                MySqlCommand command = conn.CreateCommand();
+                command.CommandText = "SELECT * FROM cars";
+                MySqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows) {
+                    while (reader.Read()) {
+                        cars.Add(new Car {
+                            Id = reader.GetInt32(0),
+                            Name = reader.GetString(1),
+                            Distance = reader.GetInt32(2),
+                            Price = reader.GetDecimal(3)
+                        });
+                    }
+                }
+            }
+            return cars;
+        }
+    }
+}
