641b24be99a503a890d54703b87e3ac7a20e223a
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-03-24 13:24:31 +0100

    Update cw18.js

--- 1TI_prog/cw18/cw18.js
+++ 1TI_prog/cw18/cw18.js
@@ -9,12 +9,23 @@ draw_sq.addEventListener("click", (event) => {
     }else{
         console.log("brak");
     }
-    ctx.fillRect(25,25,30,30);
+    const x = document.querySelector("#x").value;
+    const y = document.querySelector("#y").value;
+    ctx.beginPath();
+    ctx.strokeStyle = document.querySelector("#color").value;
+    ctx.arc(x,y,30,0,Math.PI*2,true);
+    ctx.stroke();
    // ctx.beginPath();
-   // ctx.lineWidth = "3";
-   // ctx.strokeStyle = "#4455ee";
-    ctx.strokeRect(150, 120, 30, 20);
-    //ctx.stroke();
-    const color = document.querySelector("#color");
-    console.log(color.value);
+   // ctx.lineWidth = "1";
+    // ctx.strokeStyle = "green";
+    // ctx.strokeRect(150, 120, 30, 20);
+    // ctx.stroke();
+    // ctx.strokeStyle = "blue";
+    // ctx.arc(100,100,30,0,Math.PI/2,false);
+   
+    // const color = document.querySelector("#color");
+    // ctx.moveTo(60,10);
+    // ctx.lineTo(170,75);
+    // ctx.stroke();
+    // console.log(color.value);
 });
