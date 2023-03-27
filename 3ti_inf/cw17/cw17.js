39eca9e5ac4b7ebb82f9a0f2f083cd8791c08398
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-03-27 08:44:47 +0200

    tt

---
+++ 3ti_inf/cw17/cw17.js
@@ -0,0 +1,13 @@
+const games = [
+    ["Wiedźmin 3",45.90,2015,"Fajne RPG"],
+    ["GRAT V",120.90,2015,"Fajne FPS"],
+    ["Diablo 3",66.90,2015,"Takie sobie HnS"],
+    ["FIFA 2022",210.90,2022,"Sprtowa"],
+    ["Baldurs Gate 3",170.90,2020,"Jeszcze nie wydane RPG"]
+]
+//todo
+//1. napisać Funkcję Tworzącą obiekt Game -> konstruktor
+//2. W Game napisać metodę dodającą wiersz do tabelki (dla ambitnych)
+function Game(/*...*/){
+    //...
+}
