ea49da61962c62c911cf7651d1c077ac7e330e78
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-04-12 10:24:50 +0200

    gg

--- 2tip/2tip_web/MVC_cw2/MVC_cw2.Tests/NWDUnitTest.cs
+++ 2tip/2tip_web/MVC_cw2/MVC_cw2.Tests/NWDUnitTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MVC_cw2.Models;
 
 namespace MVC_cw2.Tests
 {
@@ -11,7 +12,11 @@ namespace MVC_cw2.Tests
     {
         [TestMethod]
         public void Test1() {
-
+            NWDCounter nwd = new NWDCounter();
+            nwd.A = 20;
+            nwd.B = 15;
+            int actual = nwd.GetNWD(nwd.A, nwd.B);
+            Assert.AreEqual(5, actual);
         }
     }
 }
---
+++ 2tip/2tip_web/MVC_cw2/MVC_cw2/Models/NWDCounter.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace MVC_cw2.Models
+{
+    public class NWDCounter
+    {
+        [Display(Name = "Podaj A")]
+        [Required(ErrorMessage = "Podaj A")]
+        [Range(minimum:0,
+            maximum:Int32.MaxValue,
+            ErrorMessage = "Podaj Liczbę dodatnią")]
+        public int? A { get; set; }
+
+        [Display(Name = "Podaj B")]
+        [Required(ErrorMessage = "Podaj B")]
+        [Range(minimum: 0,
+            maximum: Int32.MaxValue,
+            ErrorMessage = "Podaj Liczbę dodatnią")]
+        public int? B { get; set; }
+        public int? Result { get; set; } = 0;
+
+        public int GetNWD(int? a, int? b) {
+            if (a != null && b != null) {
+                int temp = 0;
+                while (b!=0) {
+                    temp = (int) a;
+                    a = b;
+                    b = temp%b;
+                }
+
+                return (int)a;
+            }
+            
+            return 0;
+        }
+    }
+}
