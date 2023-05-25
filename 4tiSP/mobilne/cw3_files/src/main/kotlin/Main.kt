e70265ff85442913d08a3b6aa9a3d708c281b6c3
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-05-25 09:50:46 +0200

    hh

--- 4tiSP/mobilne/cw3_files/src/main/kotlin/Main.kt
+++ 4tiSP/mobilne/cw3_files/src/main/kotlin/Main.kt
@@ -1,5 +1,10 @@
+import java.nio.file.Paths
+
+
 fun main(args: Array<String>) {
     println("Hello World!")
+    val path = Paths.get("").toAbsolutePath().toString();
+    println(path)
     val content = listOf("ala ma kota","erw rtertert","fggdgdgd d g","gfhfghghfh");
     val fo = FileOperations();
     fo.listToFile("dane.txt",content);
