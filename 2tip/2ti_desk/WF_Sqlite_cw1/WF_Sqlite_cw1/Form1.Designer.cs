cdf7af4b667417f2a35bd54876728f2c00c7be22
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-05-29 08:34:49 +0200

    hh

--- 2tip/2ti_desk/WF_Sqlite_cw1/WF_Sqlite_cw1/Form1.Designer.cs
+++ 2tip/2ti_desk/WF_Sqlite_cw1/WF_Sqlite_cw1/Form1.Designer.cs
@@ -30,9 +30,15 @@
         {
             dataGridView1 = new DataGridView();
             dateTimePicker1 = new DateTimePicker();
-            textBox1 = new TextBox();
+            tbContent = new TextBox();
             textBox2 = new TextBox();
-            button1 = new Button();
+            btnAdd = new Button();
+            button2 = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            lLines = new Label();
+            lChars = new Label();
+            saveFileDialog1 = new SaveFileDialog();
+            btnSave = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
@@ -47,43 +53,98 @@
             // 
             // dateTimePicker1
             // 
-            dateTimePicker1.Location = new Point(547, 45);
+            dateTimePicker1.Location = new Point(479, 20);
             dateTimePicker1.Name = "dateTimePicker1";
             dateTimePicker1.Size = new Size(200, 23);
             dateTimePicker1.TabIndex = 1;
             // 
-            // textBox1
+            // tbContent
             // 
-            textBox1.Location = new Point(547, 89);
-            textBox1.Multiline = true;
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(200, 179);
-            textBox1.TabIndex = 2;
+            tbContent.Location = new Point(479, 64);
+            tbContent.Multiline = true;
+            tbContent.Name = "tbContent";
+            tbContent.ScrollBars = ScrollBars.Both;
+            tbContent.Size = new Size(200, 251);
+            tbContent.TabIndex = 2;
+            tbContent.TextChanged += tbContent_TextChanged;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(547, 306);
+            textBox2.Location = new Point(479, 332);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(200, 23);
             textBox2.TabIndex = 3;
             // 
-            // button1
+            // btnAdd
             // 
-            button1.Location = new Point(551, 357);
-            button1.Name = "button1";
-            button1.Size = new Size(196, 23);
-            button1.TabIndex = 4;
-            button1.Text = "dodaj notatkę";
-            button1.UseVisualStyleBackColor = true;
+            btnAdd.Enabled = false;
+            btnAdd.Location = new Point(483, 372);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(196, 23);
+            btnAdd.TabIndex = 4;
+            btnAdd.Text = "dodaj notatkę";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(687, 64);
+            button2.Name = "button2";
+            button2.Size = new Size(101, 55);
+            button2.TabIndex = 5;
+            button2.Text = "Załaduj notatkę";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            openFileDialog1.Filter = "tekstowe|*.txt|wszystki pliki|*.*";
+            // 
+            // lLines
+            // 
+            lLines.AutoSize = true;
+            lLines.Location = new Point(685, 149);
+            lLines.Name = "lLines";
+            lLines.Size = new Size(56, 15);
+            lLines.TabIndex = 6;
+            lLines.Text = "ilość lini: ";
+            // 
+            // lChars
+            // 
+            lChars.AutoSize = true;
+            lChars.Location = new Point(684, 185);
+            lChars.Name = "lChars";
+            lChars.Size = new Size(80, 15);
+            lChars.TabIndex = 7;
+            lChars.Text = "ilość znaków: ";
+            // 
+            // saveFileDialog1
+            // 
+            saveFileDialog1.DefaultExt = "txt";
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(687, 223);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(101, 55);
+            btnSave.TabIndex = 8;
+            btnSave.Text = "Zapisz do pliku";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button1);
+            Controls.Add(btnSave);
+            Controls.Add(lChars);
+            Controls.Add(lLines);
+            Controls.Add(button2);
+            Controls.Add(btnAdd);
             Controls.Add(textBox2);
-            Controls.Add(textBox1);
+            Controls.Add(tbContent);
             Controls.Add(dateTimePicker1);
             Controls.Add(dataGridView1);
             Name = "Form1";
@@ -98,8 +159,14 @@
 
         private DataGridView dataGridView1;
         private DateTimePicker dateTimePicker1;
-        private TextBox textBox1;
+        private TextBox tbContent;
         private TextBox textBox2;
-        private Button button1;
+        private Button btnAdd;
+        private Button button2;
+        private OpenFileDialog openFileDialog1;
+        private Label lLines;
+        private Label lChars;
+        private SaveFileDialog saveFileDialog1;
+        private Button btnSave;
     }
 }
--- 2tip/2ti_desk/WF_Sqlite_cw1/WF_Sqlite_cw1/Form1.cs
+++ 2tip/2ti_desk/WF_Sqlite_cw1/WF_Sqlite_cw1/Form1.cs
@@ -14,5 +14,36 @@ namespace WF_Sqlite_cw1
             var notes = _db.GetNotes();
             dataGridView1.DataSource = notes;
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var fileName = openFileDialog1.FileName;
+                tbContent.Text = File.ReadAllText(fileName); //fileName;
+            }
+            else
+            {
+                MessageBox.Show("Wybrano anuluj");
+            }
+        }
+
+        private void tbContent_TextChanged(object sender, EventArgs e)
+        {
+            lChars.Text = "Iloœæ znaków: " + tbContent.Text.Length.ToString();
+            lLines.Text = "Iloœæ linii:" + tbContent.Lines.Length.ToString();
+            btnAdd.Enabled = tbContent.Text.Length > 0 && tbContent.Text.Length < 1000;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e) {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK) {
+                File.WriteAllText(saveFileDialog1.FileName,tbContent.Text);
+            }
+        }
     }
 }
